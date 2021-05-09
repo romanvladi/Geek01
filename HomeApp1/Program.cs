4e004f778b2d5e8e96e12d2b91e70ec687328c02
romanvladi <romanvladi2@gmail.com>
2021-05-09 18:37:28 +0300

    Home work 03

---
+++ HomeApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeApp1
+{
+    struct Complex_Struct
+    {
+        
+    }
+    class Complex_Class
+    {
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
